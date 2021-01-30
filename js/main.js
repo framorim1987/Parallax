2d77c7998e62a885f398251213ea93a41bf8bf64
Felipe Rosa Amorim <framorim1987@gmail.com>
2021-01-29 22:12:00 -0300

    mobile

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 const pos = document.documentElement;
+const btn = document.querySelector('.btn');
+
 pos.addEventListener('mousemove', e => {
     pos.style.setProperty('--x', e.clientX + 'px');
     pos.style.setProperty('--y', e.clientY + 'px');
+});
+
+btn.addEventListener('click', () => {
+    const menu = document.querySelector('header nav ul');
+    btn.classList.toggle('close');
+    menu.classList.toggle('visible');
 });
